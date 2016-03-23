967f231e375615a0346d91ce4ce66a14ad3a2c4f
Nithya <nithya7211@gmail.com>
2016-03-23 18:31:26 +0530

    Update the news page design correction

--- js/main.js
+++ js/main.js
@@ -4,19 +4,7 @@
 
 jQuery(document).ready(function($){
 	
-    $('.frequent_questions').find('div').hide();
-
-    $('.frequent_questions ul').find('li').click(function() {
-            $(this).children('div').slideDown();
-            $(this).children('h3').addClass("showanwer");
-            // $(this).siblings().children('h3').removeClass("down_arrow");
-            $(this).siblings().children('div').slideUp();
-            $(this).siblings().children('h3').removeClass("showanwer");
-            $(this).siblings().children('h3').addClass("down_arrow");
-	});
-	
-    
-    // Owl Carousel for Latest Course
+	// Owl Carousel for Latest Course
     $('.all_latest_course').owlCarousel({
         loop:true,
         margin:30,
@@ -158,5 +146,26 @@ $("#contact_form").validate({
 		        fadeContent();
 		    });
 		}
-		
+		// Accordion for footer
+	$( "#accordion" ).accordion({
+      heightStyle: "content",
+      active:false,
+      collapsible: true,
+      header:"h3.accordianheader"
+    });
+    // Marquee 
+    $('marquee').marquee('pointer').mouseover(function () {
+		  $(this).trigger('stop');
+		}).mouseout(function () {
+		  $(this).trigger('start');
+		}).mousemove(function (event) {
+		  if ($(this).data('drag') == true) {
+		    this.scrollLeft = $(this).data('scrollX') + ($(this).data('x') - event.clientX);
+		  }
+		}).mousedown(function (event) {
+		  $(this).data('drag', true).data('x', event.clientX).data('scrollX', this.scrollLeft);
+		}).mouseup(function () {
+		  $(this).data('drag', false);
+	});
+    
 }(jQuery));	
